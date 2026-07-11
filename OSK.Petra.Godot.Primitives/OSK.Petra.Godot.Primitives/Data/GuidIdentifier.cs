61b2a86c4c480086415051a421b64475754fa07c
BlankDev117 <mmmasent@gmail.com>
2026-07-11 15:55:55 -0400

    Add initial project (OSK-1)
    Motivation
    ----
    Basic project for containing shared data or other Godot style objects that are applicable across multiple projects
    
    Modifications
    ----
    * Added project
      * Added GuidIdentifier resource
    
    Result
    ----
    Initial project for data and other types are available

---
+++ OSK.Petra.Godot.Primitives/OSK.Petra.Godot.Primitives/Data/GuidIdentifier.cs
@@ -0,0 +1,73 @@
+﻿using Godot;
+using Godot.Collections;
+using System;
+
+namespace OSK.Petra.Godot.Provisions.Data;
+
+/// <summary>
+/// A uniqiue identfiier that can be generated and reloaded within a Godot editor
+/// </summary>
+[Tool]
+[GlobalClass]
+public partial class GuidIdentifier : Resource
+{
+    #region Variables
+
+    [Export]
+    private string _id;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Generates a new empty identifier
+    /// </summary>
+    public GuidIdentifier()
+    {
+        _id = string.Empty;
+        CallDeferred(nameof(Initialize));
+    }
+
+    #endregion
+
+    #region Resource Overrides
+
+    /// <inheritdoc/>
+    public override void _ValidateProperty(Dictionary property)
+    {
+        if ((string)property["name"] == "_id")
+        {
+            property["usage"] = (int)(PropertyUsageFlags.Default | PropertyUsageFlags.ReadOnly);
+        }
+    }
+
+    #endregion
+
+    #region Api
+
+    /// <summary>
+    /// The unique identifier
+    /// </summary>
+    public Guid Id { get; private set; }
+
+    #endregion
+
+    #region Helpers
+
+    public static implicit operator Guid(GuidIdentifier identifier)
+        => identifier.Id;
+
+    private void Initialize()
+    {
+        if (string.IsNullOrWhiteSpace(_id))
+        {
+            _id = Guid.NewGuid().ToString();
+            GD.Print($"Initializing new guid identifier with Id: {_id}");
+        }
+
+        Id = Guid.Parse(_id);
+    }
+
+    #endregion
+}
