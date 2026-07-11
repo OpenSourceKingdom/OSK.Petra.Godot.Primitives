cefd67126c6631fd5e98bbf99965ea7d7a18edf7
BlankDev117 <mmmasent@gmail.com>
2026-07-11 16:24:08 -0400

    Fix Typo (OSK-2)
    Motivation
    ----
    typo in namespace
    
    Modifications
    ----
    * Corrected typo
    
    Result
    ----
    Expected output

--- src/OSK.Petra.Godot.Primitives/Data/GuidIdentifier.cs
+++ src/OSK.Petra.Godot.Primitives/Data/GuidIdentifier.cs
@@ -2,7 +2,7 @@
 using Godot.Collections;
 using System;
 
-namespace OSK.Petra.Godot.Provisions.Data;
+namespace OSK.Petra.Godot.Primitives.Data;
 
 /// <summary>
 /// A uniqiue identfiier that can be generated and reloaded within a Godot editor
